e95ee7eb91417455c22aa2726ccb5f08b1119a46
Liam Harfouche <liam.harfouche@epitech.eu>
2024-06-12 15:55:52 +0200

    [ADD]: getkeywords

--- src/file_handlers.c
+++ src/file_handlers.c
@@ -7,6 +7,7 @@
 
 #include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <string.h>
 #include <dirent.h>
 #include "libslr.h"
@@ -14,7 +15,7 @@
 struct stat *get_file_infos(char *pathname)
 {
     struct stat *statbuf;
-    
+
     if (stat(pathname, statbuf) == -1) {
         return NULL;
     }
@@ -33,11 +34,23 @@ char **get_dir_size(char *directory)
 char **get_file_list(char *directory)
 {
     size_t size = get_dir_size(directory);
-    char **l = malloc(sizeof(char *) * size);
+    char **l = malloc(sizeof(char *) * size + 1);
     DIR *dir_stream = opendir(directory);
     struct dirent *next;
 
     for (size_t i = 0; next; i++);
         l[i] = strdup(strcat(strcat(strcat("[", i + 48 ), "] ")), NEXT);
     return l;
+}
+
+char **get_keywords(char *pathname)
+{
+    int fd = open(pathname,  S_IRWXU);
+    char *line = NULL;
+    struct stat *file = get_file_infos(pathname);
+    char **keywords = NULL;
+
+   read(line, file->st_size);
+   keywords = tokn_array(line, ' ');
+   return keywords;
 }
---
+++ src/systematic_litterature_review.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** libslr [WSL: Ubuntu]
+** File description:
+** systematic_litterature_review.c
+*/
+
+#include <string.h>
+
+size_t *get_match_number(char *filename, char **keywords)
+{
+
+}
